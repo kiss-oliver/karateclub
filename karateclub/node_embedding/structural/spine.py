9a764f9169834a42bf67a92aaf7989694056fc39
Olivér Kiss <kiss_oliver@phd.ceu.edu>
2020-02-10 15:04:23 +0100

    add spine skeleton code

---
+++ karateclub/node_embedding/structural/spine.py
@@ -0,0 +1,38 @@
+import numpy as np
+import networkx as nx
+from karateclub.estimator import Estimator
+
+class spine(Estimator):
+    r"""An implementation of `"SPINE" <https://arxiv.org/pdf/1802.03984.pdf>`_
+    from the paper "SPINE: Structural Identity Preserved Inductive Network Embedding".
+
+    Args:
+        sample_number (int): Number of evaluation points. Default is 200.
+        step_size (float): Grid point step size. Default is 0.1.
+        heat_coefficient (float): Heat kernel coefficient. Default is 1.0.
+        approximation (int): Chebyshev polynomial order. Default is 100.
+        mechanism (str): Wavelet calculation method 'exact' or 'approximate'. Default is 'approximate'.
+        switch (int): Vertex cardinality when the wavelet calculation method switches to approximation. Default is 1000.
+    """
+    def __init__(self):
+
+        pass
+
+
+    def fit(self, graph):
+        """
+        Fitting a SPINE model.
+
+        Arg types:
+            * **graph** *(NetworkX graph)* - The graph to be embedded.
+        """
+        pass
+
+    def get_embedding(self):
+        r"""Getting the node embedding.
+
+        Return types:
+            * **embedding** *(Numpy array)* - The embedding of nodes.
+        """
+        embedding = None
+        return embedding
